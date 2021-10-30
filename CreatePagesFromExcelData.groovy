6c3251b2f56cc0db5a232bd33afb7a3da10a3523
Atul Kumar <akguptah@gmail.com>
2021-10-30 22:52:20 +0530

    Create CreatePagesFromExcelData.groovy

---
+++ CreatePagesFromExcelData.groovy
@@ -0,0 +1,106 @@
+ import org.apache.poi.ss.usermodel.*
+import org.apache.poi.hssf.usermodel.*
+import org.apache.poi.xssf.usermodel.*
+import org.apache.poi.ss.util.*
+import org.apache.poi.ss.usermodel.*
+import java.io.*
+import com.day.cq.commons.jcr.*;
+
+main();
+  //http://poi.apache.org/spreadsheet/quick-guide.html#Iterator
+class GroovyExcelParser {
+  def parse(path) {
+    InputStream inp = new FileInputStream(path)
+    Workbook wb = WorkbookFactory.create(inp);
+    Sheet sheet = wb.getSheetAt(0);
+
+    Iterator<Row> rowIt = sheet.rowIterator()
+    Row row = rowIt.next()
+    def headers = getRowData(row)
+
+    def rows = []
+    while(rowIt.hasNext()) {
+      row = rowIt.next()
+      rows << getRowData(row)
+    }
+    [headers, rows]
+  }
+
+  def getRowData(Row row) {
+    def data = []
+    for (Cell cell : row) {
+      getValue(row, cell, data)
+    }
+    data
+  }
+
+  def getRowReference(Row row, Cell cell) {
+    def rowIndex = row.getRowNum()
+    def colIndex = cell.getColumnIndex()
+    CellReference ref = new CellReference(rowIndex, colIndex)
+    ref.getRichStringCellValue().getString()
+  }
+ 
+  def getValue(Row row, Cell cell, List data) {
+    def rowIndex = row.getRowNum()
+    def colIndex = cell.getColumnIndex()
+    def value = ""
+    switch (cell.getCellType()) {
+      case CellType.STRING:
+        value = cell.getRichStringCellValue().getString();
+        break;
+      case CellType.NUMERIC:
+        if (DateUtil.isCellDateFormatted(cell)) {
+            value = cell.getDateCellValue();
+        } else {
+            value = cell.getNumericCellValue();
+        }
+        break;
+      case CellType.BOOLEAN:
+        value = cell.getBooleanCellValue();
+        break;
+      case CellType.FORMULA:
+        value = cell.getCellFormula();
+        break;
+      default:
+        value = ""
+    }
+    data[colIndex] = value
+    data
+  }
+
+  def toXml(header, row) {
+    def obj = "<object>\n"
+    row.eachWithIndex { datum, i -> 
+      def headerName = header[i]
+      obj += "\t<$headerName>$datum</$headerName>\n" 
+    } 
+    obj += "</object>"
+  }
+}
+  def main() {
+        def filename = '/Users/archanagupta/Desktop/announcements-data.xlsx'
+        GroovyExcelParser parser = new GroovyExcelParser()
+        def (headers, rows) = parser.parse(filename)
+        def rootPagePath = "/content/we-retail/us/en/about-us/";
+        println 'Headers'
+        println '------------------'
+        headers.each { header -> 
+          println header
+        }
+        println "\n"
+        println 'Rows'
+        println '------------------'
+        rows.each { row ->
+          String date =  row[0];
+          String time =  row[1];
+          String title =  row[2];
+          String tags =  row[3];
+          String content =  row[4];
+          
+          Page page = pageManager.create(rootPagePath,JcrUtil.createValidName(title),"we-retail/templates/content-page",title);
+          println page.path;
+          Node pageNode = page.adaptTo(Node.class);
+          Node contentNode = pageNode.getNode("jcr:content");
+        }
+  }
